6a5ab924d0cf580d3e962f4f800b2121e91e58a1
kohei sakurai <ks19ks19ks19@gmail.com>
2021-05-20 22:36:18 +0900

    add login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
   resources :pictures do
     collection do
       post :confirm
     end
   end
-  resources :users, only: [:new, :create, :show]
 end
