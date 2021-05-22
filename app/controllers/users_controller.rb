57fc0db932dd9e4f8a0853cfb219aa1459a1b9a4
kohei sakurai <ks19ks19ks19@gmail.com>
2021-05-22 09:42:07 +0900

    add edit function to user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   skip_before_action :login_required, only: [:new, :create]
+  before_action :set_user, only: %i[ show edit update destroy ]
 
   def show
-    @user = User.find(params[:id])
   end
 
   def new
@@ -18,7 +18,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: "user was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
     def user_params
       params.require(:user).permit(:image, :image_cache, :name, :email, :password, :password_confirmation)
     end
