f15c8ae8f18fbc30fb62f94891fcde92f693132b
kohei sakurai <ks19ks19ks19@gmail.com>
2021-05-26 09:09:04 +0900

    add favorites list screen and some links

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,13 @@
 class FavoritesController < ApplicationController
+  def index
+    @favorites = Favorite.all.where(user_id: current_user.id)
+  end
+
   def create
     favorite = current_user.favorites.create(picture_id: params[:picture_id])
     redirect_to pictures_path, notice: "Add #{favorite.picture.user.name}'s picture to your favorites"
   end
+
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
     redirect_to pictures_path, notice: "Remove #{favorite.picture.user.name}'s picture from your favorites"
